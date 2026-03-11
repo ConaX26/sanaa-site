f25d0ec57529eaa542d9ccb44359695fac253a88
Magnus Eriksson <din-epost@exempel.se>
2026-03-11 12:03:01 +0100

    Reduce hero image size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,7 +116,7 @@ Uppdrag över hela Sverige • För privat, företag och offentlig sektor
 <div
 className="hero-visual"
 style={{
-width: "78%",
+width: "98%",
 justifySelf: "end",
 position: "relative",
 marginRight: 24,
